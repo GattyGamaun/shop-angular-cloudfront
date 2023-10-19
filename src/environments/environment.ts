b38dc925491db75740df736ee08265d1ca8acfcb
Siarhei_Kandratsiuk <siarhei_kandratsiuk@epam.com>
2023-10-19 17:20:04 +0200

    feat: update url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment: Config = {
     product: 'https://spvhuykt6h.execute-api.eu-north-1.amazonaws.com/products',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://spvhuykt6h.execute-api.eu-north-1.amazonaws.com',
+    bff: 'https://67a22jex2e.execute-api.eu-north-1.amazonaws.com',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
